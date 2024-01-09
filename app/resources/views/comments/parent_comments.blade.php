5e8fa0a183f96b529c8b30f3c60e869e98454a39
IgorOlikov <i.olikov@mail.ru>
2024-01-09 05:31:41 +0500

    comments ugrade .

--- app/resources/views/comments/parent_comments.blade.php
+++ app/resources/views/comments/parent_comments.blade.php
@@ -4,7 +4,7 @@
         <img class="mt-2 rounded-full w-8 h-8 sm:w-10 sm:h-10" src="https://images.unsplash.com/photo-1604426633861-11b2faead63c?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=200&h=200&q=80" alt="">
     </div>
     <div class="flex-1 border rounded-lg px-4 py-2 sm:px-6 sm:py-4 leading-relaxed">
-        <strong>{{ $comment->comment_owner->name }}</strong> <span class="text-xs text-gray-400">{{ $comment->created_at }}</span>
+        <a href="users/{{ $comment->comment_owner->id }}"><strong>{{ $comment->comment_owner->name }}</strong></a> <span class="text-xs text-gray-400">{{ $comment->created_at }}</span>
         <p class="text-sm">{{ $comment->parent_id }} {{ $comment->comment }}</p>
 
         <a href="{{ route('posts.comments.children.create',[$post,$comment]) }}">Comment</a>
